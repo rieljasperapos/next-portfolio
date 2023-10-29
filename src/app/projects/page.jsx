f58a1ba8c3653b8d28e52e552ff43b7543788b57
Riel <jasper.apos@gmail.com>
2023-10-29 17:54:07 +0800

    removed margin bottom

--- src/app/projects/page.jsx
+++ src/app/projects/page.jsx
@@ -34,7 +34,7 @@ const Projects = () => {
     }
 
     return (
-        <animated.section ref={ref} style={animationProps} className="flex flex-col justify-center items-center mt-14 mb-20 sm:pr-14 sm:pl-14">
+        <animated.section ref={ref} style={animationProps} className="flex flex-col justify-center items-center mt-14 sm:pr-14 sm:pl-14">
         <h1 className="font-bold text-xl text-center sm:text-2xl lg:text-4xl mb-4 sm:mb-8 lg:mb-12 uppercase">Projects</h1>
         <div className="flex flex-col justify-center items-cente grid gap-4 lg:gap-8 sm:flex sm:flex-row flex-wrap p-4">
             {projects.map((proj, i) => (
