7c91c6c8e209975b7f7407e2a580b6630addc762
Riel <jasper.apos@gmail.com>
2023-10-29 11:17:36 +0000

    added padding

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -4,13 +4,13 @@ import texts from '@staticData/about'
 
 const about = () => {
     return (
-        <div className="mt-14 sm:pr-14 sm:pl-14">
+        <div className="mt-14 p-4 sm:pr-14 sm:pl-14">
             <div className="flex flex-col justify-center items-center">
                 <h1 className="font-bold text-xl text-center sm:text-2xl lg:text-4xl sm:pr-14 sm:pl-14 mb-16 uppercase">
                     About
                 </h1>
                 <Image src={profilepic} alt='profile'></Image>
-                <p className="leading-8 grid justify-center items-center xl:mr-64 xl:ml-64 p-16">
+                <p className="leading-8 grid justify-center items-center lg:mr-32 lg:ml-32 xl:mr-64 xl:ml-64 p-4 pt-16 sm:p-16">
                     <span className="mr-2">{texts.text1}</span>
                     <br />
                     <span>{texts.text2}</span>
