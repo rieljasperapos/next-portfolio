06856e8d89b84e8ffebdb91b1dd85ace6a7effb8
Riel <jasper.apos@gmail.com>
2023-10-29 17:54:48 +0800

    added footer

---
+++ src/components/Footer/footer.jsx
@@ -0,0 +1,23 @@
+import Link from "next/link";
+import Image from "next/image";
+import github from '../../../public/github-icon.svg'
+
+const footer = () => {
+    return (
+        <footer className="footer p-10 bg-neutral text-neutral-content mt-14">
+            <aside>
+                <p className="mt-6">@rieljasper</p>
+            </aside>
+            <nav>
+                <header className="footer-title">Socials</header>
+                <div className="grid grid-flow-col gap-4">
+                    <Link href="https://github.com/rieljasperapos" target="_blank"><Image src='/github-icon.svg' width={30} height={30}></Image></Link>
+                    <Link href='https://www.linkedin.com/in/riel-jasper-apos-4202a124a/' target="_blank"><Image src='/linkedin.png' width={30} height={30}></Image></Link>
+                    <Link href='https://www.facebook.com/rieljasper.apos/' target="_blank"><Image src='/facebook.png' width={30} height={30}></Image></Link>
+                </div>
+            </nav>
+        </footer>
+    )
+}
+
+export default footer;
