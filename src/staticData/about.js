c2072099f85d7ac736b0443218fc6f40b50ee25a
Riel <jasper.apos@gmail.com>
2023-10-29 14:04:24 +0800

    static datas for about page

---
+++ src/staticData/about.js
@@ -0,0 +1,6 @@
+const aboutme = {
+    text1: "I am currently studying Computer Science at the University of San Carlos. I aspire to become a Fullstack web developer. I'm eager to dive deep into the world of coding to build seamless and innovative web applications. The blend of front-end and back-end development excites me, and I look forward to mastering various technologies to create user-friendly and efficient digital experiences.",
+    //text2: "I'm eager to dive deep into the world of coding to build seamless and innovative web applications. The blend of front-end and back-end development excites me, and I look forward to mastering various technologies to create user-friendly and efficient digital experiences.",
+}
+
+export default aboutme;
