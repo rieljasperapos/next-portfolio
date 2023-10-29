c3c0f5cbd03476d9c7826af22b80d2caac5f8af6
Riel <jasper.apos@gmail.com>
2023-10-29 17:57:14 +0800

    added alt prop

--- src/components/Footer/footer.jsx
+++ src/components/Footer/footer.jsx
@@ -11,9 +11,9 @@ const footer = () => {
             <nav>
                 <header className="footer-title">Socials</header>
                 <div className="grid grid-flow-col gap-4">
-                    <Link href="https://github.com/rieljasperapos" target="_blank"><Image src='/github-icon.svg' width={30} height={30}></Image></Link>
-                    <Link href='https://www.linkedin.com/in/riel-jasper-apos-4202a124a/' target="_blank"><Image src='/linkedin.png' width={30} height={30}></Image></Link>
-                    <Link href='https://www.facebook.com/rieljasper.apos/' target="_blank"><Image src='/facebook.png' width={30} height={30}></Image></Link>
+                    <Link href="https://github.com/rieljasperapos" target="_blank"><Image src='/github-icon.svg' width={30} height={30} alt="github"></Image></Link>
+                    <Link href='https://www.linkedin.com/in/riel-jasper-apos-4202a124a/' target="_blank"><Image src='/linkedin.png' width={30} height={30} alt="linkedin"></Image></Link>
+                    <Link href='https://www.facebook.com/rieljasper.apos/' target="_blank"><Image src='/facebook.png' width={30} height={30} alt="facebook"></Image></Link>
                 </div>
             </nav>
         </footer>
