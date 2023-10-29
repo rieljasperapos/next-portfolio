f9043881e4efe83513993fde298732077bcdc3d4
Riel <jasper.apos@gmail.com>
2023-10-29 18:17:19 +0000

    tech stack progress bar

--- src/app/tech/page.jsx
+++ src/app/tech/page.jsx
@@ -18,6 +18,7 @@ import Tailwind from '../../../public/tailwindcss.png'
 import Bootstrap from '../../../public/bootstrap.png'
 import text from "@staticData/tech";
 import Image from "next/image";
+import techStack from "@/staticData/techStack";
 
 function Techs() {
     const [ref, inView] = useInView({
@@ -46,47 +47,19 @@ function Techs() {
         <h1 className="font-bold text-xl text-center sm:text-2xl lg:text-4xl sm:pr-14 sm:pl-16 sm:ml-2 sm:mb-8 lg:mb-16 mt-14 uppercase">Technologies</h1>
         <section className="grid p-4 pb-16 sm:pb-22 sm:pr-14 sm:pl-14 xl:ml-64 xl:mr-64 h-full mt-8">
             <div>
-                <div className="flex flex-wrap w-auto justify-center lg:pt-12 sm:pr-12">
-                    <div className="group">
-                        <Image src={C} alt="C" id="C" className="w-10 h-10 m-4 sm:w-16 sm:h-16 hover:scale-105 hover:animate-bounce transition-transform duration-300 ease-in"></Image>
-                        <p className=" font-mono text-center sm:opacity-0 group-hover:opacity-100 transition-opacity ease-in duration-200">C</p>
-                    </div>
-                    <div className="group">
-                        <Image src={Java} alt='Java' id="Java" className="w-10 h-10 m-4 sm:w-16 sm:h-16 hover:scale-105 hover:animate-bounce transition-transform duration-300 ease-in"></Image>
-                        <p className=" font-mono text-center sm:opacity-0 group-hover:opacity-100 transition-opacity ease-in duration-200">Java</p>
-                    </div>
-                    <div className="group">
-                        <Image src={Javascript} alt='Javascript' id="Javascript" className="w-10 h-10 m-4 sm:w-16 sm:h-16 hover:scale-105 hover:animate-bounce transition-transform duration-300 ease-in"></Image>
-                        <p className=" font-mono text-center sm:opacity-0 group-hover:opacity-100 transition-opacity ease-in duration-200">Javascript</p>
-                    </div>
-                    <div className="group">
-                        <Image src={react} alt='react' id="react" className="w-12 h-10 m-4 sm:w-20 sm:h-16 hover:scale-105 hover:animate-bounce transition-transform duration-300 ease-in"></Image>
-                        <p className=" font-mono text-center sm:opacity-0 group-hover:opacity-100 transition-opacity ease-in duration-200">React</p>
-                    </div>
-                    <div className="group">
-                        <Image src={Tailwind} alt='tailwind' id="tailwind" className="w-10 h-10 m-4 sm:w-16 sm:h-16 hover:scale-105 hover:animate-bounce transition-transform duration-300 ease-in"></Image>
-                        <p className=" font-mono text-center sm:opacity-0 group-hover:opacity-100 transition-opacity ease-in duration-200">Tailwind</p>
-                    </div>
-                    <div className="group">
-                        <Image src={Css} alt='CSS' id="CSS" className="w-10 h-10 m-4 sm:w-16 sm:h-16 hover:scale-105 hover:animate-bounce transition-transform duration-300 ease-in"></Image>
-                        <p className=" font-mono text-center sm:opacity-0 group-hover:opacity-100 transition-opacity ease-in duration-200">Css</p>
-                    </div>
-                    <div className="group">
-                        <Image src={Bootstrap} alt='Bootstrap' id="Bootstrap" className="w-10 h-10 m-4 sm:w-16 sm:h-16 hover:scale-105 hover:animate-bounce transition-transform duration-300 ease-in"></Image>
-                        <p className=" font-mono text-center sm:opacity-0 group-hover:opacity-100 transition-opacity ease-in duration-200">Bootstrap</p>
-                    </div>
-                    <div className="group">
-                        <Image src={Html} alt='Html' id="Html" className="w-10 h-10 m-4 sm:w-16 sm:h-16 hover:scale-105 hover:animate-bounce transition-transform duration-300 ease-in"></Image>
-                        <p className=" font-mono text-center sm:opacity-0 group-hover:opacity-100 transition-opacity ease-in duration-200">Html</p>
-                    </div>
-                    <div className="group">
-                        <Image src={Php} alt='Php' id="Php" className="w-10 h-10 m-4 sm:w-16 sm:h-16 hover:scale-105 hover:animate-bounce transition-transform duration-300 ease-in"></Image>
-                        <p className=" font-mono text-center sm:opacity-0 group-hover:opacity-100 transition-opacity ease-in duration-200">Php</p>
-                    </div>
-                    <div className="group">
-                        <Image src={Mysql} alt='Mysql' id="Mysql" className="w-16 h-10 m-4 sm:w-24 sm:h-16 lg:w-24 lg:h-14 hover:scale-105 hover:animate-bounce transition-transform duration-300 ease-in"></Image>
-                        <p className=" font-mono text-center sm:opacity-0 group-hover:opacity-100 transition-opacity ease-in duration-200">Mysql</p>
-                    </div>
+                <div className="flex flex-wrap w-auto justify-center gap-5 lg:pt-12 sm:pr-12">
+                    {techStack.map((item, indx) => {
+                        return (
+                            <div key={indx} className="group">
+                                <div className={`hover:scale-105 hover:animate-bounce transition-transform duration-300 ease-in shadow radial-progress ${item.color}`} style={{ "--value": `${item.value}`, "--size": "10rem", "--thickness": "1rem" }}>
+                                    <Image src={item.src} width={40} height={40} alt={item.title}></Image>
+                                    <span className="font-bold mt-2">{item.progress}</span>
+                                </div>
+                                <p className="group-hover:opacity-100 transition-opacity duration-200 ease-300 opacity-0 text-center mt-4">{item.title}</p>
+                            </div>
+        
+                        )
+                    })}
                 </div>
             </div>
             <div className='flex flex-col p-4 pt-0 w-auto mb-8'>
---
+++ src/staticData/techStack.js
@@ -0,0 +1,83 @@
+const techStack = [
+    {
+        src: "/C.png",
+        title: 'C',
+        value: 90,
+        progress: '90%',
+        color: "text-blue-500",
+    },
+
+    {
+        src: "/java.png",
+        title: 'Java',
+        value: 60,
+        progress: '60%',
+        color: "text-red-500",
+    },
+
+    {
+        src: "/JS.png",
+        title: 'Javascript',
+        value: 70,
+        progress: '70%',
+        color: "text-yellow-500",
+    },
+
+    {
+        src: "/react.png",
+        title: "React",
+        value: 50,
+        progress: "50%",
+        color: "text-blue-300",
+    },
+
+    {
+        src: "/tailwindcss.png",
+        title: 'Tailwind',
+        value: 90,
+        progress: '90%',
+        color: "text-blue-400",
+    },
+
+    {
+        src: "/CSS.png",
+        title: 'CSS',
+        value: 70,
+        progress: '70%',
+        color: "text-blue-700",
+    },
+
+    {
+        src: "/bootstrap.png",
+        title: 'Bootstrap',
+        value: 40,
+        progress: '40%',
+        color: "text-primary",
+    },
+
+    {
+        src: "/HTML.png",
+        title: 'HTML',
+        value: 80,
+        progress: '80%',
+        color: "text-orange-500",
+    },
+
+    {
+        src: "/php.png",
+        title: 'Php',
+        value: 40,
+        progress: '40%',
+        color: "text-purple-300",
+    },
+
+    {
+        src: "/mysql.png",
+        title: 'Mysql',
+        value: 50,
+        progress: '50%',
+        color: "text-green-500",
+    },
+]
+
+export default techStack;
