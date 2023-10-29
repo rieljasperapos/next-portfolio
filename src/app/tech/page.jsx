b6ffd2bb05acf57336946d53a86a3479b05d8553
Riel <jasper.apos@gmail.com>
2023-10-29 17:54:37 +0800

    changed breakpoint to xl

--- src/app/tech/page.jsx
+++ src/app/tech/page.jsx
@@ -44,7 +44,7 @@ function Techs() {
     return (
         <animated.div ref={ref} style={animationProps}>
         <h1 className="font-bold text-xl text-center sm:text-2xl lg:text-4xl sm:pr-14 sm:pl-16 sm:ml-2 sm:mb-8 lg:mb-16 mt-14 uppercase">Technologies</h1>
-        <section className="grid p-4 pb-16 sm:pb-22 sm:pr-14 sm:pl-14 lg:ml-64 lg:mr-64 h-full mt-8">
+        <section className="grid p-4 pb-16 sm:pb-22 sm:pr-14 sm:pl-14 xl:ml-64 xl:mr-64 h-full mt-8">
             <div>
                 <div className="flex flex-wrap w-auto justify-center lg:pt-12 sm:pr-12">
                     <div className="group">
@@ -89,7 +89,7 @@ function Techs() {
                     </div>
                 </div>
             </div>
-            <div className='flex flex-col p-4 w-auto mt-6'>
+            <div className='flex flex-col p-4 w-auto mt-6 mb-8'>
                 <p className=" text-xl">{text.p1}</p>
                 <p className=" text-xl mt-8">{text.p2}</p>
             </div>
