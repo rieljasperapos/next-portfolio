11e48d71226a1efedebdb6329b57ef8ce7228e80
Riel <jasper.apos@gmail.com>
2023-10-30 07:37:28 +0000

    updated tailwind config to include static datas

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,6 +4,7 @@ module.exports = {
     './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
     './src/components/**/*.{js,ts,jsx,tsx,mdx}',
     './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/**'
   ],
   theme: {
     extend: {
@@ -11,6 +12,13 @@ module.exports = {
         'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
         'gradient-conic':
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      },
+      colors: {
+        'c': '#005B99',
+        'javascript': '#F0DC4E',
+        'css': '#2453E5',
+        'php': '#7977B0',
+        'bootstrap': '#7511F5'
       },
     },
   },
