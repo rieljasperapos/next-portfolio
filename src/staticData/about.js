6e72b5ba6ea03de2b2efe9bac06c0d2efc6a70e1
Riel <jasper.apos@gmail.com>
2023-10-30 14:06:35 +0800

    rephrase

--- src/staticData/about.js
+++ src/staticData/about.js
@@ -1,5 +1,5 @@
 const aboutme = {
-    text1: "I am currently studying Computer Science at the University of San Carlos. I aspire to become a Fullstack web developer. I'm eager to dive deep into the world of coding to build seamless and innovative web applications. The blend of front-end and back-end development excites me, and I look forward to mastering various technologies to create user-friendly and efficient digital experiences.",
+    text1: "I am currently studying at University of San Carlos taking up Bachelor of Science in Computer Science. I'm eager to dive deep into the world of coding to build seamless and innovative web applications. The blend of front-end and back-end development excites me, and I look forward to explore various technologies to create user-friendly and efficient digital experiences.",
     text2: "Beyond the realms of Fullstack development, my curiosity extends to the cutting-edge domains of data science and machine learning. The prospect of unraveling patterns in data, building predictive models, and contributing to the evolving landscape of artificial intelligence captivates my interest.",
 }
 
